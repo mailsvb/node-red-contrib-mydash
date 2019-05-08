b83e4c02cab31dc6ffc45b88b0cda6b3888f1486
mailsvb <mailsvb@gmail.com>
2019-05-08 17:03:03 +0200

    use DHCP messages as trigger

--- README.md
+++ README.md
@@ -3,14 +3,12 @@ A node-red node for receiving Amazon Dash-Button pressed events.
 
 ## Installation
 npm install -g node-red-contrib-mydash
-```
-### requires tcpdump and expect to be installed on the operating system.
-apt-get install expect tcl8.6 tcpdump
-pacman -S expect tcpdump
-```
+
+## Requirements
+The script binds on UDP port 67 for receiving DHCP broadcast events. Make sure your node-red instance has the privileges to access UDP port 67.
 
 ## Features
-- send event when a specified Amazon Dash-Button is pressed
+send event when a specified Amazon Dash-Button is pressed
  
 ## Configuration
-- provide MAC address of Dash-Button
+provide MAC address of Dash-Button
--- mydash.js
+++ mydash.js
@@ -1,35 +1,14 @@
 const spawn = require('child_process').spawn;
-var unbufferExec = require('child_process').spawnSync('which', ['unbuffer'], {encoding: 'utf8'});
-var tcpdumpExec = require('child_process').spawnSync('which', ['tcpdump'], {encoding: 'utf8'});
-if (unbufferExec.stdout != "" || tcpdumpExec.stdout != "")
-{
-    unbufferExec = unbufferExec.stdout.replace(/\n/, '');
-    tcpdumpExec = tcpdumpExec.stdout.replace(/\n/, '');
-}
-const filter = 'arp[6:2]&0x0001=1';
-var scan = spawn(unbufferExec, [tcpdumpExec, '-t', '-e', '-n', '-q', '-K', '-U', '-Q', 'In', '-i', 'any', filter]);
+const dgram = require('dgram')
+const server = dgram.createSocket('udp4')
+const listening = new Promise((resolve) => {
+    server.on('listening', resolve)
+    server.bind(67)
+})
 
-const getTcpDumpProcess = (node) =>
+const formatMacAddress = (raw) =>
 {
-    node.log(`getTcpDumpProcess() mac[${node.mac}]`);
-    if (unbufferExec != "" || tcpdumpExec != "")
-    {
-        if (scan && scan.killed === true)
-        {
-            node.log(`tcpdump killed, restarting`);
-            scan = spawn(unbufferExec, [tcpdumpExec, '-t', '-e', '-n', '-q', '-K', '-U', '-Q', 'In', '-i', 'any', filter]);
-            return;
-        }
-        else
-        {
-            node.log(`tcpdump alive. pid[${scan.pid}]`);
-            return;
-        }
-    }
-    else
-    {
-        node.error(`error starting tcpdump. unbuffer[${unbufferExec}] tcpdump[${tcpdumpExec}]`);
-    }
+    return [raw.substr(0, 2), raw.substr(2, 2), raw.substr(4, 2), raw.substr(6, 2), raw.substr(8, 2), raw.substr(10, 2)].join(':')
 }
 
 module.exports = (RED) =>
@@ -46,53 +25,30 @@ module.exports = (RED) =>
             node.status({fill:`red`,shape:`dot`,text:`no MAC given`});
             return;
         }
-        getTcpDumpProcess(node);
         node.regex  = new RegExp(node.mac);
-
-        if (scan && scan.killed === false)
+        
+        server.on('message', (msg, rinfo) =>
         {
-            node.log('tcpdump alive, attaching event listener');
-            node.status({fill:`green`,shape:`ring`,text:`waiting ${node.mac}`});
-            scan.stdout.on("data", (d) =>
+            var data = formatMacAddress(msg.toString('hex', 28, 34));
+            if (data.match(node.regex))
             {
-                var data = d.toString();
-                if (data.match(node.regex))
+                node.log(`event detected for mac: ${data}`);
+                if (node.fired == false)
                 {
-                    if (node.fired == false)
-                    {
-                        node.fired = true;
-                        node.send({mac: node.mac});
-                        setTimeout(() => node.fired = false, 2000);
-                    }
-                    else
-                    {
-                        node.log('ignore second arp request within 2 seconds');
-                    }
+                    node.fired = true;
+                    node.send({mac: node.mac});
+                    setTimeout(() => node.fired = false, 2000);
                 }
-            });
-            scan.on("close", () =>
-            {
-                if (scan.killed === true)
+                else
                 {
-                    node.error('tcpdump killed');
-                    node.status({fill:`red`,shape:`dot`,text:`tcpdump stopped`});
+                    node.log('ignore second request within 2 seconds');
                 }
-            });
-        }
-        else
-        {
-            node.error(`tcpdump not running, will not attach event listener`);
-            node.status({fill:`red`,shape:`dot`,text:`tcpdump not running`});
-        }
+            }
+        })
         
         node.on("close", () =>
         {
             node.status({});
-            if (scan.killed === false)
-            {
-                node.log(`stopping tcpdump`);
-                scan.kill();
-            }
         });
     }
     RED.nodes.registerType("mydash-node",MyDashNode);
